751a414f3389abaa01af6ac3c9b66337a4ea543b
Cristian Bula <cristian.bula.dev@gmail.com>
2024-07-05 17:41:38 -0500

    ajustes de skills

--- src/constants/proyects.ts
+++ src/constants/proyects.ts
@@ -1,28 +1,30 @@
 import MindWell from '../assets/imgs/mindWell.webp'
+import Css2Color from '../assets/imgs/css2Color.webp'
+import Gotuuri from '../assets/imgs/gotuuri.webp'
 
 export const PROYECTS=[
     {
         id:1,
-        name:"IconJSX",
-        technologies:"Next JS, Typescript, Tailwind CSS",
-        img: 'https://picsum.photos/seed/hola/200/300?grayscale',
-        description:"Optimizador de iconos svg",
-        url:"XXXXXXXXXXXXXXXXXXXXXX",
+        name:"Gotuuri",
+        technologies:"Vite JS, Typescript, Tailwind CSS, Express, Mongo DB",
+        img: Gotuuri,
+        description:"Gotuuri es una plataforma que conecta a viajeros con guías locales, para ofrecer experiencias auténticas en cada destino.",
+        url:"https://gotuuri.com",
     },
     {
         id:2,
         name:"Convert target color",
         technologies:"Vite JS, Typescript, Tailwind CSS",
-        img: 'https://picsum.photos/seed/como/200/300?grayscale',
+        img: Css2Color,
         description:"Convertir un filtro de css en un color",
-        url:"XXXXXXXXXXXXXXXXXXXXXX"
+        url:"https://css2-color.vercel.app/"
     },
     {
         id:3,
         name:"Mind Well",
         technologies:"Next JS, Typescript, Node JS, Mongo DB",
         img: MindWell,
-        description:"Chatbot con ia usando la api de Open Ai, enfocado en la salud mental",
+        description:"Chatbot con ia usando la api de Open Ai, enfocado en la salud mental, (diseñado para dispositivos moviles)",
         url:"https://mind-well.vercel.app/"
     }
     
